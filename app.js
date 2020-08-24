a346747faa7fcf2cbe24f6ded6a5b19b8beccd5a
SirPm <odumosuoluwapm@gmail.com>
2020-08-24 09:27:58 +0100

    Fixed footer

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function generateMeal() {
             <p class="c-and-r"> Category: ${data.meals[0].strCategory}</p>
             <p class="c-and-r"> Region: ${data.meals[0].strArea}</p>
             <img src="${data.meals[0].strMealThumb}">
-            <p class="list-heading">List of Ingredients: </p>
+            <p class="list-heading"><strong>List of Ingredients</strong></p>
         `;
 
         for(let i = 1; i <= 20; i++ ) {
